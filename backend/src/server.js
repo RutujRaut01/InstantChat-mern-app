4936197c033e106c9413df77754a86b4d8b471e2
Rutuj Raut <rautrutuj123@gmail.com>
2025-11-04 00:18:34 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,30 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+console.log("Serving static files from:", path.join(__dirname, "../frontend/dist"));
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+    app.get("*", (_, res) =>{
+        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
+    });
+}
 
 app.listen(PORT, ()=> console.log("Server running on port "+ PORT));
