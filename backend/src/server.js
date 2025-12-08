165813b4a16930c261ad527a50be527350c68e00
Your Name <you@example.com>
2025-12-08 23:19:50 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-console.log("Serving static files from:", path.join(__dirname, "../frontend/dist"));
+// Dynamically locate frontend build
+const frontendPath = path.resolve(__dirname, "../frontend/dist");
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../../frontend/dist")));
-    app.get("*", (_, res) =>{
-        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(frontendPath));
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(frontendPath, "index.html"));
     });
 }
 
-app.listen(PORT, ()=> console.log("Server running on port "+ PORT));
+
+
+connectDB().then(() => {
+    app.listen(PORT, () => console.log("Server running on port " + PORT));
+});
