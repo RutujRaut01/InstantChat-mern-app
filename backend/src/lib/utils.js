1b7cab90a29d7fe9817b71daaa3f366efe9947c6
Your Name <you@example.com>
2025-12-09 23:34:23 +0530

    Signup Integration

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 export const generateToken = (userId, res) =>{
 
     const { JWT_SECRET } = process.env;
-    if(JWT_SECRET) {
+scm-history-item:c%3A%5CUsers%5CRUTUJ%20RAUT%5CDesktop%5CInstantChat?%7B%22repositoryId%22%3A%22scm0%22%2C%22historyItemId%22%3A%22165813b4a16930c261ad527a50be527350c68e00%22%2C%22historyItemParentId%22%3A%22a233f899cc7eaeadc0e44950e1dd0fc28b8ebc2e%22%2C%22historyItemDisplayId%22%3A%22165813b%22%7D    if(JWT_SECRET) {
         throw new Error("JWT_SECRET is not configured.");
     }
 
