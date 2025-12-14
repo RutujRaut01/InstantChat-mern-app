0c2f5195ed36f82f93d25dbaa4205621a19a0244
Your Name <you@example.com>
2025-12-14 15:13:46 +0530

    login & logout functionality add

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -78,3 +78,46 @@ export const signup = async (req, res) =>{
         res.status(500).json({message: "Internal server error"});
     }
 };
+
+
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.findOne({email});
+        if(!user)
+        {
+            return res.status(400).json({message: "Invalid Credentials"});
+            // Best Practice Followed: Never tell the client which one is incorrect: password or username  
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        // user.password is stored in db in hashed form
+        // brcypt extracts salt from stored password and then encrypts entered plain text password with same salt and algorithm
+        // then compares both hashed values, if equal return true else false
+        if(!isPasswordCorrect)
+        {
+            return res.status(400).json({message: "Invalid Credentials"});
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    }
+    catch(error)
+    {
+        console.error("Error in login controller", error);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+
+};
+
+
+export const logout = (_, res) => {
+    res.cookie("jwt", "", {maxAge: 0});
+    res.status(200).json({message: "Logged out successfully"});
+};
