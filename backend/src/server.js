93b94fc2d7f1e8373649e370b6d6b9caafa9f633
Your Name <you@example.com>
2025-12-14 16:06:14 +0530

    update dynamic frontend path

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,8 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // Dynamically locate frontend build
-const frontendPath = path.resolve(__dirname, "../frontend/dist");
+const frontendPath = path.resolve(__dirname, "../../frontend/dist");
+
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(frontendPath));
